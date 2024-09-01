e5ad118a4ca855b27e2ffa72853ff8646a9d2b3a
emirozturk <emirozturk3@gmail.com>
2024-09-01 20:32:23 +0300

    Changed pdf import

--- query_data.py
+++ query_data.py
@@ -5,42 +5,39 @@ from langchain_ollama import OllamaEmbeddings
 from langchain_ollama import ChatOllama
 from langchain.prompts import ChatPromptTemplate
 
-CHROMA_PATH = "chroma"
 
-PROMPT_TEMPLATE = """
-Answer the question based only on the following context:
-
-{context}
-
----
-
-Answer the question based on the above context: {question}
-"""
+PROMPT_TEMPLATE = """Answer the question using only on the given context:
+                {context}
+                ---
+                Answer the question based on the above context: 
+                {question}
+                """
 
 
-def main():
-    query_text = "Who is emir öztürk"
-
+def main(db_path):
     embedding_function = OllamaEmbeddings(model="llama3")
-    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
+    db = Chroma(persist_directory=db_path, embedding_function=embedding_function)
 
-    # Search the DB.
-    results = db.similarity_search_with_relevance_scores(query_text, k=3)
-    if len(results) == 0 or results[0][1] < 0.1:
-        print(f"Unable to find matching results.")
-        return
+    while(True):
+        input = input("Query:") 
+        if input == ":exit":
+            break
 
-    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
-    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
-    prompt = prompt_template.format(context=context_text, question=query_text)
+        results = db.similarity_search_with_relevance_scores(input, k=3)
+        if len(results) == 0:
+            print("Sorry, can't find a result")
+        else:
+            context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+            prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+            prompt = prompt_template.format(context=context_text, question=input)
 
-    model = ChatOllama(model="llama3")
-    response_text = model.predict(prompt)
+            model = ChatOllama(model="llama3")
+            response_text = model.predict(prompt)
 
-    sources = [doc.metadata.get("source", None) for doc, _score in results]
-    formatted_response = f"Response: {response_text}\nSources: {sources}"
-    print(formatted_response)
-
-
-if __name__ == "__main__":
-    main()
+            docs = [doc.metadata.get("source", None) for doc, _score in results]
+            response = f"Response: {response_text}\nFrom: {docs}"
+            print(response)
+        
+        
+db_path = "/Chroma"
+main(db_path)
--- update_database.py
+++ update_database.py
@@ -1,39 +1,15 @@
-# from langchain.document_loaders import DirectoryLoader
-from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema import Document
 from langchain_ollama import OllamaEmbeddings
 from langchain_community.vectorstores import Chroma
-from dotenv import load_dotenv
 import os
-import shutil
-
-
-CHROMA_PATH = "chroma"
-
-
-def main():
-    generate_data_store()
-
-
-def generate_data_store():
-    documents = load_documents()
-    chunks = split_text(documents)
-    save_to_chroma(chunks)
-
-
-def load_documents():
-    loader = DirectoryLoader("/Users/emirozturk/Desktop/Data", glob="*.txt")
-    documents = loader.load()
-    return documents
 
 
 def split_text(documents: list[Document]):
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=300,
-        chunk_overlap=100,
-        length_function=len,
-        add_start_index=True,
+        chunk_size=1000,
+        chunk_overlap=200,
     )
     chunks = text_splitter.split_documents(documents)
     print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
@@ -41,24 +17,32 @@ def split_text(documents: list[Document]):
     return chunks
 
 
-def save_to_chroma(chunks: list[Document]):
-    # Check if the database already exists
-    if os.path.exists(CHROMA_PATH):
-        # Load the existing database
-        db = Chroma(persist_directory=CHROMA_PATH, embedding_function=OllamaEmbeddings(model="llama3"))
-        print(f"Loaded existing database from {CHROMA_PATH}.")
+def save_to_chroma(db_path,chunks: list[Document]):
+    if os.path.exists(db_path):
+        db = Chroma(persist_directory=db_path, embedding_function=OllamaEmbeddings(model="llama3"))
+        print(f"Loaded existing database from {db_path}.")
+        db.add_documents(chunks)
     else:
-        # Create a new database
-        db = Chroma.from_documents(
-            chunks, OllamaEmbeddings(model="llama3"), persist_directory=CHROMA_PATH
-        )
-        print(f"Created new database and saved {len(chunks)} chunks to {CHROMA_PATH}.")
-    
-    # Add new documents to the existing or new database
-    db.add_documents(chunks)
+        db = Chroma.from_documents(chunks, OllamaEmbeddings(model="llama3"), persist_directory=db_path)
+        print(f"Created new database and saved {len(chunks)} chunks to {db_path}.")
     db.persist()
-    print(f"Added {len(chunks)} chunks to the database at {CHROMA_PATH}.")
+    print(f"Added {len(chunks)} chunks to the database at {db_path}.")
+    
+
+def get_documents(path):
+    files=os.listdir(path)
+    list_of_documents = []
+    for file in files:
+        loader = PyPDFLoader(os.path.join(path,file))
+        documents = loader.load()
+        list_of_documents.extend(documents)
+    return list_of_documents
 
 
-if __name__ == "__main__":
-    main()
+db_path = "/Chroma"
+docs_path = "c:/Users/emiro/Desktop/pdf/"
+
+
+documents = get_documents(docs_path)
+chunks = split_text(documents)
+save_to_chroma(db_path,chunks)
